4015fb9057cce71a83e9247badd6f64cc4d30b62
RenatoSenai <renato.giacomo@sp.senai.br>
2025-03-12 13:45:51 -0300

    Minimal changes

--- src/features/analysis/pages/List/index.tsx
+++ src/features/analysis/pages/List/index.tsx
@@ -1,22 +1,21 @@
 import { createRoute } from '@tanstack/react-router'
 import { PrivateRoutes } from '@shared/layouts'
-import AnalysisFilters from '@features/analysis/components/FIlters'
+import AnalysisFilters from '@features/analysis/components/Filters'
 import AnalysisList from '@features/analysis/components/List'
-
-
 
 function ListAnalysisPage() {
   return (
     <>
       <div className="flex justify-between items-center">
         <div className="flex flex-col gap-8 pb-4">
-            <div>
-              <h1 className="text-2xl font-bold text-neutral-900">Analises</h1>
-              <p className="text-gray-500">
-                Gerencie todos os exercícios da plataforma ou crie novos
-              </p>
-            </div>
+          <div>
+            <h1 className="text-2xl font-bold text-neutral-900">Análises</h1>
+            <p className="text-gray-500">
+              Gerencie as análises de produtos que os consumidores enviaram.
+            </p>
+          </div>
         </div>
+        <AnalysisFilters />
       </div>
       <AnalysisList />
     </>
