1d5dc3a7ab55cddc2589eeec8b6e28faca72786f
RenatoSenai <renato.giacomo@sp.senai.br>
2025-04-02 16:55:00 -0300

    Refactor pagination component to dynamically generate page buttons and improve state management

--- src/features/users/components/List/Pagination/index.tsx
+++ src/features/users/components/List/Pagination/index.tsx
@@ -1,6 +1,5 @@
 import { UsersData } from '@features/users/services'
-import { Button } from '@istic-ui/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 interface UserPaginationProps {
   page: number | undefined
@@ -15,34 +14,49 @@ function UserPagination({
   pageCount,
   setPageCount,
 }: UserPaginationProps) {
+  const [totalPages, setTotalPages] = useState<number[]>([])
+
+  {
+    /*
+    !!Info sobre as variáveis!!
+    'pagination' é o retorno da API, que contém informações sobre a paginação.
+    'page' é o número da página atual.
+    'pageCount' é o número total de páginas.
+   */
+  }
+
   useEffect(() => {
-    console.warn(pagination?.pageCount)
-    console.warn(page)
-    console.warn(pagination)
+    const TotalPagesArray = []
+
+    if (pagination?.pageCount) {
+      for (let i = 1; i <= pagination?.pageCount; i++) {
+        TotalPagesArray.push(i)
+        setTotalPages(TotalPagesArray)
+        console.warn(totalPages)
+      }
+    }
+    
+
   }, [pageCount])
+
+  console.warn(totalPages)
 
   return (
     <div className="flex justify-center align-middle gap-2 items-center">
-      
-      <button>
-        <span className="text-sm text-gray-500">
-          {pagination?.page} de {pagination?.pageCount}
-        </span>
-      </button>
 
-      <Button
-        onClick={() => setPageCount(1)}
-        variant="outline"
-        label="1"
-        size="xs"
-      />
 
-      <Button
-        onClick={() => setPageCount(2)}
-        variant="outline"
-        label="2"
-        size="sm"
-      />
+      {totalPages.map((pageIndex, index) => (
+        <button
+          key={index}
+          onClick={() => setPageCount(pageIndex)}
+          disabled={page === pageIndex}
+          className={` py-1 px-3 rounded-md border-zinc-600 border-2 ${
+            page === pageIndex ? 'bg-zinc-600 text-white' : 'text-zinc-500'
+          }`}
+        >
+          {pageIndex}
+        </button>
+      ))}
     </div>
   )
 }
